6f6443bc63a85a304df60afd082988a9cbe1af5f
Chris Nixon <chrisnxn@gmail.com>
2017-04-04 13:58:22 -0700

    Add appId as parameter to server side integration (#34)
    * populating app_id with apiKey from settings. updated tests
    
    * added test cases for no apiKey in settings
    
    * populating app_id with apiKey from settings. updated tests
    
    * added test cases for no apiKey in settings
    
    * Updated test fixtures with new appboy id
    
    * Release commit

--- test/index.js
+++ test/index.js
@@ -12,6 +12,7 @@ describe('Appboy', function(){
   beforeEach(function(){
     settings = {
       appGroupId: 'd54c3d18-71d5-4ebc-8a41-5cfae902977f',
+      apiKey: 'my_api_key',
       trackPages: true,
       updateExistingOnly: false,
       datacenter: 'us'
@@ -96,6 +97,11 @@ describe('Appboy', function(){
         settings.updateExistingOnly = true;
         test.maps('track-update-existing-only');
       });
+
+      it('should leave app_id blank when apiKey is not in settings', function(){
+        delete settings.apiKey;
+        test.maps('track-basic-no-api-key');
+      });
     });
 
     describe('orderCompleted', function(){
@@ -105,6 +111,11 @@ describe('Appboy', function(){
 
       it('should use default currency and quantity for products when they do not exist', function(){
         test.maps('track-products-defaults');
+      });
+
+      it('should leave app_id blank when apiKey is not in settings', function(){
+        delete settings.apiKey;
+        test.maps('track-basic-no-api-key');
       });
     });
 
